365718f98887a315d90754905b428959b38e69bf
andreclimaco <andre.climaco@gmail.com>
2013-08-19 17:22:47 -0300

    Frontend

--- frontend/etc/vhosts/generateConf.sh
+++ frontend/etc/vhosts/generateConf.sh
@@ -3,15 +3,16 @@
 
 TEMPLATE="../templates/portal.conf.in"
 VAR_PORTALID=$1
+VAR_PLONE_SITE_ID=$2
 VAR_MIRROR_NGINX="../../var/www/$VAR_PORTALID/mirror"
 VAR_TMP_NGINX="../../var/www/$VAR_PORTALID/tmp"
 
-if [ $1 ]; then
+if [ $1 -a $2 ]; then
     if [ -f $TEMPLATE ]; then
         if [ -f ./$VAR_PORTALID.conf ]; then
             echo "O arquivo $VAR_PORTALID.conf já existe.";
         else
-            cat $TEMPLATE | sed -e s/VAR_PORTALID/$VAR_PORTALID/g | tee ./$VAR_PORTALID.conf 1>/dev/null;
+            cat $TEMPLATE | sed -e s/VAR_PORTALID/$VAR_PORTALID/g | sed -e s/VAR_PLONE_SITE_ID/$VAR_PLONE_SITE_ID/g | tee ./$VAR_PORTALID.conf 1>/dev/null;
             echo "O arquivo $VAR_PORTALID.conf foi criado com sucesso!";
             if [ ! -d $VAR_MIRROR_NGINX ]; then
                 mkdir -p $VAR_MIRROR_NGINX;
